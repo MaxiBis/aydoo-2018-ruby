a936f4bcc8ddf20e7e8b831c6013e6c990ea6302
MAXIMILIANO FERNANDO BISCARDI <biscardi@ar.ibm.com>
2018-05-27 23:41:39 -0300

    ambos sentidos implementados

--- fibo5/spec/sentido_spec.rb
+++ fibo5/spec/sentido_spec.rb
@@ -1,18 +1,28 @@
 require 'rspec'
 require_relative '../model/fibonacci'
+require_relative '../model/sentido'
+require_relative '../model/sentido_directo'
+require_relative '../model/sentido_inverso'
 
 class Sentido_spec
 
   describe 'sentidoTests' do
 
     let(:fibo) { Fibonacci.new }
-    let(:sentido)
 
     it 'aplico un sentido directo y devuelve lo mismo' do
       sentido = SentidoDirecto.new
-      salidaOriginal = fibo.generar(5)
+      salidaOriginal = [0,1,1,2,3]
 
       expect(sentido.aplicarSobre(salidaOriginal)).to eq salidaOriginal
+    end
+
+    it 'aplico un sentido inverso y devuelve la lista al revez' do
+      sentido = SentidoInverso.new
+      salidaOriginal = [0,1,1,2,3]
+      salidaEsperada = [3,2,1,1,0]
+
+      expect(sentido.aplicarSobre(salidaOriginal)).to eq salidaEsperada
     end
 
     #it 'genero una susecion de numero negativo' do
